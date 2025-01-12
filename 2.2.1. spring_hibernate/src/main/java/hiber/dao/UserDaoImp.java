2d0d4ef887b7eda15ffbd19d5db2cac283793570
Ob-3 <shashev1870@gmail.com>
2025-01-13 00:48:45 +0700

    Working application

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,11 +1,14 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
+import javax.persistence.Query;
+
 import java.util.List;
 
 @Repository
@@ -24,6 +27,14 @@ public class UserDaoImp implements UserDao {
    public List<User> listUsers() {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
+   }
+
+   @Override
+   public User UserModelSeries(String model, int series) {
+      Session session = sessionFactory.openSession();
+      Query query = session.createQuery("select u from User u,Car c where u.car.id = c.id and c.model = :modelCar and" +
+              " c.series = :seriesCar");
+      return (User) query.setParameter("modelCar", model).setParameter("seriesCar", series).getSingleResult();
    }
 
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -2,6 +2,7 @@ package hiber.service;
 
 import hiber.dao.UserDao;
 import hiber.model.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,6 +25,12 @@ public class UserServiceImp implements UserService {
    @Override
    public List<User> listUsers() {
       return userDao.listUsers();
+   }
+
+   @Transactional
+   @Override
+   public User userCar(String model, int series) {
+      return userDao.UserModelSeries(model, series);
    }
 
 }
